2f5e9f110629b4ac63abd5ee2b630db6db77c652
Cyomu <113843040+Cyomu@users.noreply.github.com>
2024-05-24 09:17:38 +0300

    main

--- model/apilog.py
+++ model/apilog.py
@@ -1,4 +1,4 @@
 from requests import get
-treatment = input('Вид операции (0=хирургическая, 1=нефролитотомия): ')
-stone_size = input('Размер камня (0=большой, 1=маленький): ')
+treatment = input('Вид операции (0 - Хирургическая, 1 - Нефролитотомия): ')
+stone_size = input('Размер камня (0 - Большой, 1 - Маленький): ')
 print(get(f'http://127.0.0.1:5000/apilog', json={'treatment': treatment, 'stone_size': stone_size}).json())
--- model/apilr.py
+++ model/apilr.py
@@ -1,5 +1,5 @@
 from requests import get
-TV = input('телевизионная реклама: ')
-radio = input('реклама на радио: ')
-newspaper = input('реклама в газетах: ')
+TV = input('Телевизионная реклама: ')
+radio = input('Реклама на радио: ')
+newspaper = input('Реклама в газетах: ')
 print(get(f'http://127.0.0.1:5000/apilr', json={'TV': TV, 'radio': radio, 'newspaper': newspaper}).json())
